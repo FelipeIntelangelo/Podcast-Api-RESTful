36318f906a747323db7b3074e11d39f3eb049753
Felipe Intelangelo <felipeintelangelo@gmail.com>
2025-05-23 22:16:40 -0300

    fix

---
+++ PodcastProject/src/main/java/podcast/entities/Commentary.java
@@ -0,0 +1,25 @@
+package podcast.entities;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Commentaries")
+public class Commentary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "episode_id", nullable = false)
+    private Episode episode;
+}
---
+++ PodcastProject/src/main/java/podcast/entities/enums/Category.java
@@ -0,0 +1,11 @@
+package podcast.entities.enums;
+
+public enum Category {
+    TECNOLOGIA,
+    NEGOCIOS,
+    ENTRETENIMIENTO,
+    DEPORTES,
+    EDUCACION,
+    SALUD,
+    ARTE
+}
